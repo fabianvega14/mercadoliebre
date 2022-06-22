983765fe9e5ce42cd1fcc6e41f2388544e7aaba4
unknown <fabianvega140882@gmail.com>
2022-06-22 09:01:08 -0300

    modificaciones

---
+++ Desktop/mercadoliebre/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+app.use(express.static("public"));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/home.html'));
+});
+
+
+
+
+app.listen(3000, () => {
+    console.log('Servidor iniciado en: http://localhost:3000');
+})
